5f91d4c76141218f510c25634a4659b9329b3c2f
Wojtek <wojciechopitz@gmail.com>
2024-11-16 14:06:32 +0100

    first commit

---
+++ appka/src/components/Message/Message.jsx
@@ -0,0 +1,11 @@
+import Alert from "@mui/material/Alert";
+
+export const Message = () => {
+  return (
+    <div>
+      <Alert severity="success">
+        Here is a gentle confirmation that your action was successful.
+      </Alert>
+    </div>
+  );
+};
